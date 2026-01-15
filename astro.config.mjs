c53c5b0da7008f1c35d8a2d344a6c7cc934c8fbe
VladyslavOmelyanenko <36175376+VladyslavOmelyanenko@users.noreply.github.com>
2026-01-15 23:46:33 +0100

    update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import sanity from "@sanity/astro";
 import { defineConfig } from "astro/config";
-
+import netlify from "@astrojs/netlify";
 import react from '@astrojs/react';
 
 
 
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   transitions: {
     enabled: true,
   },
