ce5bf8903a66cd86c41001ae0679edbb37fe5175
VladyslavOmelyanenko <36175376+VladyslavOmelyanenko@users.noreply.github.com>
2026-02-05 19:29:27 +0100

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,22 @@
 import sanity from "@sanity/astro";
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
-
 
 export default defineConfig({
   output: "static",
   adapter: netlify(),
   site: "https://dzastins.com",
-  integrations: [sitemap()],
-  transitions: {
-    enabled: true,
-  },
+  transitions: { enabled: true },
+
   integrations: [
     sanity({
       projectId: "saq01wqu",
       dataset: "production",
-      // Set useCdn to false if you're building statically.
       useCdn: false,
     }),
     react(),
+    sitemap(),
   ],
 });
